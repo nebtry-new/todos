bb5e117d5cf4dcd2fb3813c4c40038f8b66fcdca
EC2 Default User <ec2-user@ip-172-31-21-213.ap-southeast-1.compute.internal>
2018-04-19 18:39:21 +0000

    initial version 1

---
+++ models/index.js
@@ -0,0 +1,9 @@
+var mongoose = require("mongoose");
+mongoose.set('debug', true);
+
+var url = process.env.DATABASEURL || "mongodb://localhost/todo-api";
+mongoose.connect(url);
+
+mongoose.Promise = Promise;
+
+module.exports.Todo = require("./todo");
